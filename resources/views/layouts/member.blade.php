8fd7ca668f3cd0ea3787c051e093a168cebb3765
nakajin <ichika.wookun1286@gmail.com>
2022-05-28 09:25:45 +0900

    fix:footer修正

--- resources/views/layouts/member.blade.php
+++ resources/views/layouts/member.blade.php
@@ -90,7 +90,7 @@
     </section>
     <footer class="footer text-faded text-center py-5">
         <div class="container">
-            <p class="m-0 small">Copyright &copy; Your Website 2022</p>
+          <p class="m-0 small">Copyright &copy; kyomuzo 2022</p>
         </div>
     </footer>
     <!-- Bootstrap core JS-->
--- resources/views/layouts/store2.blade.php
+++ resources/views/layouts/store2.blade.php
@@ -105,7 +105,7 @@
             </div>
         </section>
         <footer class="footer text-faded text-center py-5">
-          <div class="container"><p class="m-0 small">Copyright &copy; Your Website 2022</p></div>
+          <div class="container"><p class="m-0 small">Copyright &copy; kyomuzo 2022</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
